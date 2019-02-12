d1f8cf15b22703cfbd4225eb49b10691a918a087
ogata-k <ogtkzk712@gmail.com>
2019-02-12 19:14:52 +0900

    実行可能な状況まで編集

--- example_gtk/src/main.rs
+++ example_gtk/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+extern crate gtk;
+
+use gtk::prelude::*;
+use gtk::{Button, Label, Window, WindowType};
+
+fn main(){
+  if gtk::init().is_err() {
+    println!("Faild to initialize GTK");
+    return;
+  }
+
+  // windowの作成
+  let window = Window::new(WindowType::Toplevel);
+  window.set_title("カウントアップ");
+  window.set_default_size(400, 300);  // 横×縦
+
+  // 各widgetの作成
+  let label = Label::new("0");
+  let button = Button::new_with_label("+1");
+  
+  // windowへの配置
+  let vbox = gtk::Box::new(gtk::Orientation::Vertical, 2);
+  // child: &P, expand: bool, fill: bool, padding: u32
+  vbox.pack_start(&label, true, true, 3); 
+  vbox.pack_start(&button, false, true, 2);
+  window.add(&vbox);
+  
+  // 初期表示
+  window.show_all();
+
+  // eventの設定
+  window.connect_delete_event(|_, _| {
+    gtk::main_quit();
+    Inhibit(false)
+  });
+
+  button.connect_clicked(|_| {
+    println!("カウント+1");
+  });
+
+  // GUIの実行
+  gtk::main();
 }
+
