5724755bfa5c3ef6bb3d11d34a65734741e099f5
ogata-k <ogtkzk712@gmail.com>
2019-02-24 18:20:14 +0900

    eventの記述を翻訳するためにevent管理関係の翻訳をした

--- example_conrod/src/main.rs
+++ example_conrod/src/main.rs
@@ -130,6 +130,7 @@ fn set_widgets(ref mut ui: conrod::UiCell, ids: &mut Ids, num: &mut String) {
 
 }
 
+// イベントの管理用構造体
 struct EventLoop {
     ui_needs_update: bool,
     last_update: std::time::Instant,
@@ -143,37 +144,35 @@ impl EventLoop {
         }
     }
 
-    /// Produce an iterator yielding all available events.
+    /// すべての更新対象となるイベントの為の順次取得用関数
     pub fn next(&mut self, display: &glium::Display) -> Vec<glium::glutin::Event> {
-        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
-        // since the last yield.
+        // 60FPSより早くならないようにするために一つ前の更新対象から少なくても16ms待つことにしておく。
         let last_update = self.last_update;
         let sixteen_ms = std::time::Duration::from_millis(16);
-        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);  // 前回の更新時と今の時間差の取得
         if duration_since_last_update < sixteen_ms {
             std::thread::sleep(sixteen_ms - duration_since_last_update);
         }
 
-        // Collect all pending events.
+        // イベント全体の取得
         let mut events = Vec::new();
-        events.extend(display.poll_events());
+        events.extend(display.poll_events());  // displayにおけるイベントの取得
 
-        // If there are no events and the `Ui` does not need updating, wait for the next event.
+        // displayで更新があればUiでのイベント更新は次に持ち越し
         if events.is_empty() && !self.ui_needs_update {
             events.extend(display.wait_events().next());
         }
 
+        // イベントの更新の後処理
         self.ui_needs_update = false;
         self.last_update = std::time::Instant::now();
 
         events
     }
 
-    /// Notifies the event loop that the `Ui` requires another update whether or not there are any
-    /// pending events.
-    ///
-    /// This is primarily used on the occasion that some part of the `Ui` is still animating and
-    /// requires further updates to do so.
+    // Uiで他のイベントの更新があるかないかを要求することをeventのループでは確認しておくこと
+
+    // これはいくつかのUiを描画する最初のタイミングや更新を要求するタイミングで使われる。
     pub fn needs_update(&mut self) {
         self.ui_needs_update = true;
     }
