30300a8103d87b55728be6afd5bf54614a7cdc4a
ogata-k <ogtkzk712@gmail.com>
2019-03-05 17:15:38 +0900

    Windowのサイズ変更が成功

--- example_azul/src/main.rs
+++ example_azul/src/main.rs
@@ -1,7 +1,7 @@
 extern crate azul;
 
 use azul::{prelude::*, widgets::{label::Label, button::Button}};
-
+use azul::window_state::WindowSize;
 // レイアウトに依存するデータモデルの定義
 struct DataModel {
   count_num: usize,
@@ -37,23 +37,16 @@ fn main() {
   // 引数はレイアウトを決定する初期条件とログやエラー処理に関するデータ構造
   let app = App::new(DataModel {count_num: 0}, AppConfig::default());
 
-  /*
-  let window_setting = WindowState{
-          size: WindowSize{
-            dimensions: LogicalSize::new(400.0, 300.0),
-            ..Default::default()
-          },
-          title: "count up".to_string(),
-          ..Default::default()
-  };
-  let window_create_options = WindowCreateOptions{
-    state: window_setting,
-    ..Default::default()
-  };
-  */
+  // Windowの設定
+  let mut window_options = WindowCreateOptions::default();
+  window_options.state.title = "カウントアップ".to_string();
+  let mut window_size = WindowSize::default();
+  window_size.dimensions = LogicalSize::new(400.0, 300.0);  // width * height
+  window_options.state.size = window_size;
 
+  // CSSの設定
   macro_rules! CSS_PATH { () => (concat!(env!("CARGO_MANIFEST_DIR"), "/src/style.css")) }
 
   let css = css::override_native(include_str!(CSS_PATH!())).expect(&format!("failed: override CSS by {}", CSS_PATH!()));
-  app.run(Window::new(WindowCreateOptions::default(), css).expect("failed: make window")).expect("failed: start running application");
+  app.run(Window::new(window_options, css).expect("failed: make window")).expect("failed: start running application");
 }
